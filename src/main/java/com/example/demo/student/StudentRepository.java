92ea414700b1e2b34c0c9b52b16ca702a1dad1b7
Amal <amalgovinds.mec@gmail.com>
2024-10-16 00:17:46 +0530

    Add new user by email

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 //Data Access Layer
 //JpaRepo syntax refers to obj to be referred and its ID
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
